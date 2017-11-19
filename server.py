5091960a5660688442d9a51b6c68b32597eaed82
Daniel <daniel.castellanom@gmail.com>
2017-11-19 07:00:02 -0800

    Logica Unity-Server

--- server.py
+++ server.py
@@ -1,26 +1,29 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, render_template, request,jsonify,json
 from mongoengine import *
 
 app = Flask(__name__)
 
 connect('test')
 
-class Player(Document):
-    id = StringField(required=True,max_length=25)
-    xPosition = StringField(max_length=50)
-    yPosition = StringField(max_length=50)
+
+
+class Player(EmbeddedDocument):
+    playerId = StringField(required=True, max_length=36)
+    tankLife = IntField(min_value=1)
+
+class GameSession(EmbeddedDocument):
+    gameId = StringField(required=True,max_length=36)
+    players = ListField(EmbeddedDocumentField(Player))
+    gameState = StringField(max_length=10)
+    gameWinner = StringField(max_length=25)
+
+class Items(Document):
+    gameSession = ListField(EmbeddedDocumentField(GameSession))
 
 class Action(Document):
-    playerId = StringField(required=True,max_length=25)
+    playerId = StringField(required=True,max_length=36)
     actionId = IntField(min_value=1)
     actionType = StringField(required=True,max_length=10)
-    movementCords = StringField(max_length=10)
-
-class unityTest(Document):
-    tankLife = IntField(min_value=1)
-    playerId = StringField(required=True, max_length=25)
-    actionId = IntField(min_value=1)
-    actionType = StringField(required=True, max_length=10)
     movementCords = StringField(max_length=10)
 
 
@@ -28,30 +31,28 @@ class unityTest(Document):
 def hello():
     return "Hello world"
 
-@app.route('/testPost', methods=['POST'])
+@app.route('/gameSessionInit', methods=['POST'])
 def testPost():
-    test = request.get_json()
-    print(test)
-    createTest(test)
+    test = request.form.to_dict()
+    loaded_r = {}
+    for key, value in test.items():
+        dict = key
+        loaded_r = json.loads(key)
+    print(loaded_r['Items'][0]['gameState'])
+    print(loaded_r ['Items'][0]['players'])
     return "DATA OK"
 
 @app.route('/testGet/<playerId>', methods=['GET'])
 def testGet(playerId):
-    test = unityTest.objects(playerId = playerId)
-    print(test)
+
     return "DATA GET"
 
 
-def createTest(jsonData):
-    action = unityTest(
-        tankLife = jsonData["tankLife"],
-        playerId = jsonData["playerId"],
-        actionId = jsonData["actionId"],
-        actionType = jsonData["actionType"],
-        movementCords = jsonData["movementCords"],
-
+def createGameSession(jsonData):
+    session = Items(
+        gameSession = jsonData["Items"]
     )
-    action.save()
+    session.save()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
